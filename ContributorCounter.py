349f77782fe68fa29c682a09a4c2cfd98e7fe419
Mark Olson <mlo@baskerville.org>
2018-07-27 10:08:07 -0400

    Fix issues with printing sorted lists

--- ContributorCounter.py
+++ ContributorCounter.py
@@ -21,14 +21,21 @@ for i in range(0, len(alphabet)-1):     # First letter
                 subdirs=[join(dir, f2) for f2 in listdir(dir) if os.path.isdir(join(dir, f2))]
                 for subdir in subdirs:      # Versions
                     e=ET.parse(os.path.join(subdir, "metadata.xml")).getroot()
-                    id=e.find("name").text
+                    id=e.find("name").text.strip()
                     if id in userCounts.keys():
                         userCounts[id]=userCounts[id]+1
                     else:
                         userCounts[id]=1
 
 
+# Print a listing sorted by user ID
 keys=userCounts.keys()
 keys=sorted(keys)
+print("\n\n\nListing by user\n")
 for key in keys:
     print(key+": "+str(userCounts[key]))
+
+# Print a listing sorted by count
+print("\n\n\nListing by count\n")
+for w in sorted(userCounts, key=userCounts.get, reverse=True):
+  print(w+":  "+str(userCounts[w]))
